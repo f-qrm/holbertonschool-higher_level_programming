46ab09b36222e40b6749f8a51e1bab47b3467f03
f-qrm <qerimifjolla7@gmail.com>
2025-05-12 16:14:20 +0200

    fix: change or for and

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = my_string.replace("c", "")
-    new_string = my_string.replace("C", "")
+    new_string = ""
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            new_string += char
     return new_string
