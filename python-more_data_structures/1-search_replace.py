e64493dc26d342f8fbc22ab35bad85cb5e19d4ff
f-qrm <qerimifjolla7@gmail.com>
2025-05-13 09:59:07 +0200

    feat: add function to replace elements in a list without modifying the original

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
+    new1_list = []
+    for i in my_list:
+        if i == search:
+            new1_list.append(replace)
+        else:
+            new1_list.append(i)
+    return new1_list
