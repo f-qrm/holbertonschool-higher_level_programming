1c46748d7eaf8f46bda16dbdef83c0e02da5756a
f-qrm <qerimifjolla7@gmail.com>
2025-05-13 13:52:49 +0200

    style: spaces

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    for cle in sorted (a_dictionary):
-        print (f"{cle}: {a_dictionary[cle]}")
+    for cle in sorted(a_dictionary):
+        print(f"{cle}: {a_dictionary[cle]}")
