34f2afcbcfd8fb34fe0f78ec501edb3a375d39e3
f-qrm <qerimifjolla7@gmail.com>
2025-05-14 10:18:01 +0200

    fix: change the first condition if for the function isinstance to be able to verify correctly

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not roman_string or roman_string is None:
+    if not isinstance(roman_string, str):
         return 0
     new_dict = {
         'I': 1,
