3e1fa178a1014de36a2bdfc19ff236435bef5dae
f-qrm <qerimifjolla7@gmail.com>
2025-05-15 09:29:43 +0200

    style: blank line

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -4,6 +4,8 @@ This function adds two integers and returns their sum.
 The second argument `b` has a default value of 98.
 If either argument is not an integer or a float, a TypeError is raised.
 """
+
+
 def add_integer(a, b=98):
     """
     Adds two integers a and b.
