8690e0e87a0fd1ce9549681664e23cfd84ef3f3d
f-qrm <qerimifjolla7@gmail.com>
2025-05-15 22:11:25 +0200

    style: comments

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
+"""
+This module defines the function say_my_name.
+"""
+
+
 def say_my_name(first_name, last_name=""):
+    """
+    say_my_name prints a full name based on the given arguments.
+    Raises a TypeError if either argument is not a string.
+    Displays: My name is <first_name> <last_name>
+    """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
