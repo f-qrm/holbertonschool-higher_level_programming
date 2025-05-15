c3a5aea92a73b80a82010ee6197831996349405b
f-qrm <qerimifjolla7@gmail.com>
2025-05-15 23:32:32 +0200

    fix: spaces befor the line and new line

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,26 +1,20 @@
 #!/usr/bin/python3
-"""
-This module defines the function text_indentation.
-"""
-
 
 def text_indentation(text):
-    """
-    Print a text with two new lines after each '.', '?', or ':' character.
-    Removes any spaces following these characters before continuing.
-    TypeError: If text is not a string.
-    """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
+
     characters = ['.', '?', ':']
     i = 0
-    longeur = len(text)
-    while i < longeur:
-        print(text[i], end='')
+    length = len(text)
+    while i < length and text[i] == ' ':
+        i += 1
+    while i < length:
+        print(text[i], end="")
         if text[i] in characters:
-            print("\n\n", end='')
+            print()
             i += 1
-            while i < longeur and text[i] == ' ':
+            while i < length and text[i] == ' ':
                 i += 1
             continue
         i += 1
