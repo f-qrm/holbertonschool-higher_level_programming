f18f586b426b689b15656d5000fadbcd0d4153b1
f-qrm <qerimifjolla7@gmail.com>
2025-05-19 10:16:56 +0200

    feat: Creation of an empty class

---
+++ python-classes/0-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This module defines a class named Square.
+
+The Square class is an empty class that represents a geometric square.
+It serves as a foundation for learning about classes and object-oriented programming in Python.
+"""
+
+
+class Square:
+    """Represents a square.
+
+    This is a basic class that currently does not contain any attributes or methods.
+    It will be expanded in later projects to include more functionalities such as size and area.
+    """
+    pass
