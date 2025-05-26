c852a77b4bff4b47a648219600248cb8dc53de90
f-qrm <qerimifjolla7@gmail.com>
2025-05-26 23:26:46 +0200

    fix: remove the last modification  self.__size = size in the methode init self size

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -96,6 +96,7 @@ class Square(Rectangle):
         """
         self.integer_validator("size", size)
         super().__init__(size, size)
+        self.__size = size
 
     def area(self):
         """
