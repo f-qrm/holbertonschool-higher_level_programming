60e7df62c4174c04649fe652ad71f7a43c743459
f-qrm <qerimifjolla7@gmail.com>
2025-05-28 10:45:27 +0200

    fix: add return item to the methode pop

--- python-abc/task_02_verboselist.py
+++ python-abc/task_02_verboselist.py
@@ -30,7 +30,7 @@ class VerboseList(list):
             Added [10] to the list.
         """
         super().append(item)
-        print(f"Added {[item]} to the list.")
+        print(f"Added [{item}] to the list.")
 
     def extend(self, x):
         """
@@ -47,7 +47,7 @@ class VerboseList(list):
             Extended the list with [2] items.
         """
         super().extend(x)
-        print(f"Extended the list with {[len(x)]} items.")
+        print(f"Extended the list with [{len(x)}] items.")
 
     def remove(self, item):
         """
@@ -63,7 +63,7 @@ class VerboseList(list):
             Removed [2] from the list.
         """
         super().remove(item)
-        print(f"Removed {[item]} from the list.")
+        print(f"Removed [{item}] from the list.")
 
     def pop(self, i=-1):
         """
@@ -84,3 +84,4 @@ class VerboseList(list):
         """
         item = super().pop(i)
         print(f"Popped {[item]} from the list.")
+        return item
