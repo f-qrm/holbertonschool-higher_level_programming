b8a88d12ccf24eea05a2ff3855921092249775f0
f-qrm <qerimifjolla7@gmail.com>
2025-06-04 13:15:03 +0200

    fix: two new lines before the class

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -4,6 +4,7 @@ This module provides a function that returns the dictionary description
 of an object for JSON serialization.
 """
 
+
 def class_to_json(obj):
     """
     Returns the dictionary description of an object for JSON serialization.
