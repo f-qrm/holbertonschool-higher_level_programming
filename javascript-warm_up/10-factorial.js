93673177d0795ef7f7298eebd5ef03d89a02e14b
f-qrm <qerimifjolla7@gmail.com>
2025-07-11 20:24:11 +0200

    feat: add script to compute factorial recursively

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-const argv = parseInt(process.argv[2]);
 
 function factorial(n) {
   if (isNaN(n) || n <= 1) {
@@ -7,4 +6,5 @@ function factorial(n) {
   }
   return n * factorial(n - 1);
 }
-console.log(factorial(argv));
+const arg = parseInt(process.argv[2]);
+console.log(factorial(arg));
