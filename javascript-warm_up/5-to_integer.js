dbac921b769dfcc05cc57235040171d421b9cd2d
f-qrm <qerimifjolla7@gmail.com>
2025-07-11 16:57:02 +0200

    fix: add ; for console.log

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -4,5 +4,5 @@ const arg = process.argv[2];
 if (isNaN(arg) || !arg) {
   console.log('Not a number');
 } else {
-  console.log(`My number: ${parseInt(arg)}`)
+  console.log(`My number: ${parseInt(arg)}`);
 }
