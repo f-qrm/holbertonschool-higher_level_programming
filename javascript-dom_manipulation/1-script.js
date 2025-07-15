a0943939b88a1953bf734b8f4b3b8072c2a3975c
f-qrm <qerimifjolla7@gmail.com>
2025-07-15 09:41:09 +0200

    fix: # before the code color

--- javascript-dom_manipulation/1-script.js
+++ javascript-dom_manipulation/1-script.js
@@ -2,5 +2,5 @@
 const redHeader = document.querySelector('#red_header');
 const header = document.querySelector('header');
 redHeader.addEventListener('click', function () {
-  header.style.color = 'FF0000';
+  header.style.color = '#FF0000';
 });
